c513e3b7b5687194220f7b4e050ab39bc408e16e
frankgomezdev <franklingomezdev@gmail.com>
2026-02-24 11:23:25 -0500

    fix: base in config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Replace 'edh-dashboard' with your actual GitHub repo name
-  base: '/edh-dashboard/',
+  base: '/',
 })
